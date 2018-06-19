207a47ccfd03a00616ebd268bf0b1e211200df7d
Nicole Pretorius <nicole.pretoriusuk@gmail.com>
2018-06-19 14:49:20 +0000

    automatically backed up by learn

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -1,12 +1,11 @@
-
-
-def display_board(board)
+def display_board
+  cell = "   "
   separator = "|"
   lines = "-----------"
 
-  puts "#{board[0]}   #{separator}   #{board[1]}   #{separator}   #{board[2]}"
-  puts "lines"
-  puts "#{board[3]}   #{separator}   #{board[4]}   #{separator}   #{board[5]}"
-  puts "lines"
-  puts "#{board[6]}   #{separator}   #{board[7]}   #{separator}   #{board[8]}"
+  puts "#{cell}#{separator}#{cell}#{separator}#{cell}"
+  puts "#{lines}"
+  puts "#{cell}#{separator}#{cell}#{separator}#{cell}"
+  puts "#{lines}"
+  puts "#{cell}#{separator}#{cell}#{separator}#{cell}"
 end
